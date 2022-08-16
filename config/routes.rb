126321eef52e0a26d1ed65bcd80ca8cc912a676d
Luis Enrique Guzman Pineda <luis.guzp13@gmail.com>
2022-08-16 13:02:36 -0500

    create controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "faker"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,6 +7,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-10.times do
-  Post.create(title: Faker::Movie.title, details: Faker::Movie.quote, completed: false)
-end
+# 10.times do
+#   Task.create(title: Faker::Movie.title, details: Faker::Movie.quote)
+# end
